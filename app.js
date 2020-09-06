22b96a562c86c4b4f7f8294c143e114296b7141d
Stward Pérez Cabezas <stwardperezc@estudiantec.cr>
2020-09-05 18:41:54 -0600

    GraphQL added

--- app.js
+++ app.js
@@ -1,11 +1,47 @@
+// IMPORTS
 const express = require("express");
 const body_parser = require("body-parser");
+const { graphqlHTTP } = require("express-graphql");
+const { buildSchema } = require("graphql");
 
 const app = express();
 
 app.use(body_parser.json());
 
-app.get("/", (req, res, next) => {
-  res.send("Running Express");
-});
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: buildSchema(`
+      type RootQuery {
+        notes: [String!]!
+      }
+      
+      type RootMutation {
+        createNote(body: String): String
+      }
+      
+      schema{
+        query: RootQuery 
+        mutation: RootMutation
+      }
+    `),
+    rootValue: {
+      // this is the bundle of resolvers
+      notes: () => {
+        return [
+          "This is for tomorrow",
+          "Make dinner",
+          "Study this next week",
+          "Gotta remember to sleep",
+        ];
+      },
+      createNote: (args) => {
+        const noteBody = args.body;
+        return noteBody;
+      },
+    },
+    graphiql: true, // this is for a UI and test your schemas
+  })
+);
+
 app.listen("3000");
